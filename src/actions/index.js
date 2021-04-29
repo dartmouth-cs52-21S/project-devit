a8a08da5d4604926f994dc3bc144b3d4d210b7a9
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-04-29 15:44:04 -0400

    added redux

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+export const ActionTypes = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+};
+
+export function increment() {
+  return {
+    type: ActionTypes.INCREMENT,
+    payload: null,
+  };
+}
+
+export function decrement() {
+  return {
+    type: ActionTypes.DECREMENT,
+    payload: null,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router, Route, Switch, NavLink,
 } from 'react-router-dom';
-import Welcome from './components/welcome';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
 import About from './components/about';
 import Test from './components/test';
 import FallBack from './components/fallback';
+import Controls from './components/controls';
 import './style.scss';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
 
 const App = (props) => {
   console.log('app is rendering');
@@ -23,7 +33,7 @@ const App = (props) => {
           </ul>
         </nav>
         <Switch>
-          <Route exact path="/" component={Welcome} />
+          <Route exact path="/" component={Controls} />
           <Route path="/about" component={About} />
           <Route exact path="/test/:id" component={Test} />
           <Route component={FallBack} />
@@ -33,4 +43,9 @@ const App = (props) => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
+);
