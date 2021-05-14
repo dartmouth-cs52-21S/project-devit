c23f625303a2af7a51c9c27bebd64d4377795bae
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-05-14 17:27:48 -0400

    root url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://platform-api-lab-5.herokuapp.com/api';
 const API_KEY = '';
 
 export const ActionTypes = {
