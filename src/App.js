c5396ff9bdb49592689e4fc18dbdd7e38e7f9914
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-21 13:47:07 -0400

    Merge branch 'master' into feature/sidebar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Posts from './components/posts';
 import NewPost from './components/new-post';
 import SinglePost from './components/single-post';
-import SignIn from './components/sign-in';
+import SignIn from './components/SignIn';
 import PrivateRoute from './components/private-route';
 import Profile from './components/profile';
 import Banner from './components/Banner';
 import Sidebar from './components/Sidebar';
 import ErrorNotFound from './components/ErrorNotFound';
+import SignUp from './components/SignUp';
 
 const App = () => {
   return (
@@ -24,8 +25,9 @@ const App = () => {
               <PrivateRoute path="/posts/new" component={NewPost} />
               <Route exact path="/posts/:postID" component={SinglePost} />
               <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
               <Route path="/profile" component={Profile} />
-              <Route path="*" component={ErrorNotFound} />
+              <Route component={ErrorNotFound} />
             </Switch>
           </div>
         </main>
---
+++ src/components/SignUp.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { signupUser } from '../store/actions';
+
+const SignUp = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    location: '',
+    picture: '',
+    bio: '',
+    roles: [],
+    skills: [],
+  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  return (
+    <div className="sign-up">
+      <h1>Account Info</h1>
+      <ul>
+        <li><h2>Email:<span id="red">*</span> </h2></li>
+        <li><input type="text" value={user.email} onChange={(e) => setUser({ email: e.target.value })} /></li>
+        <li><h2>Password:<span id="red">*</span> </h2></li>
+        <li><input type="password" value={user.password} onChange={(e) => setUser({ password: e.target.value })} /></li>
+      </ul>
+      <h1>User Info</h1>
+      <ul>
+        <li><h2>First Name:<span id="red">*</span> </h2></li>
+        <li><input type="text" value={user.firstName} onChange={(e) => setUser({ ...user, firstName: e.target.value })} /></li>
+        <li><h2>Last Name:<span id="red">*</span> </h2></li>
+        <li><input type="text" value={user.lastName} onChange={(e) => setUser({ ...user, lastName: e.target.value })} /></li>
+      </ul>
+      <ul>
+        <li><h2>Profile Pic: </h2></li>
+        <li><input type="text" value={user.picture} onChange={(e) => setUser({ ...user, picture: e.target.value })} /></li>
+        <li><h2>Location: </h2></li>
+        <li><input type="text" value={user.location} onChange={(e) => setUser({ ...user, location: e.target.value })} /></li>
+      </ul>
+      <ul>
+        <li><h2>Bio: </h2></li>
+        <li><textarea type="text" value={user.bio} onChange={(e) => setUser({ ...user, bio: e.target.value })} /></li>
+      </ul>
+      <ul>
+        <li><h2>Roles: </h2></li>
+        <li>
+          <h3>Developer</h3><input type="checkbox" name="dev" onClick={() => setUser({ ...user, roles: [...user.roles, 'dev'] })} />
+          <h3>Designer</h3> <input type="checkbox" name="designer" onClick={() => setUser({ ...user, roles: [...user.roles, 'designer'] })} />
+          <h3>Idea Person</h3> <input type="checkbox" name="idea" onClick={() => setUser({ ...user, roles: [...user.roles, 'idea'] })} />
+        </li>
+      </ul>
+      <ul>
+        <li><h2>Skills: </h2></li>
+        <li>
+          <h3>React</h3><input type="checkbox" name="react" onClick={() => setUser({ ...user, skills: [...user.skills, 'react'] })} />
+          <h3>HTML/CSS</h3> <input type="checkbox" name="html/css" onClick={() => setUser({ ...user, skills: [...user.skills, 'html/css'] })} />
+        </li>
+      </ul>
+      <button type="button"
+        onClick={() => dispatch(signupUser(user, history))}
+      >Sign Up
+      </button>
+    </div>
+  );
+};
+
+export default SignUp;
