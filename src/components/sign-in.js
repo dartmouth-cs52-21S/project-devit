05f292d8a57f4976ff222e63845e070895706acf
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-05-21 00:57:13 -0400

    sign in page styled

--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { signinUser, signupUser } from '../actions/index';
+import { signinUser } from '../actions/index';
 
 const SignIn = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [author, setAuthor] = useState('');
 
-  console.log(author);
   return (
     <div className="sign-in">
-      <h1>Sign In or Sign Up</h1>
-      <ul>
-        <li><h2>Email: </h2><input type="text" value={email} onChange={(e) => setEmail(e.target.value)} /></li>
-        <li><h2>Password: </h2><input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /></li>
-        <li><h2>Name: </h2><input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} /></li>
-      </ul>
-      <button type="button" onClick={() => props.signinUser({ email, password }, props.history)}>Sign In</button>
-      <button type="button" onClick={() => props.signupUser({ email, password, author }, props.history)}>Sign Up</button>
+      <div className="container">
+        <img src="../../images/logo/devit_logo_light.png" alt="DevIt Logo" />
+        <ul>
+          <li><h2>Email<span id="red">*</span> </h2></li>
+          <li><input type="text" value={email} onChange={(e) => setEmail(e.target.value)} /></li>
+          <li><h2>Password<span id="red">*</span> </h2></li>
+          <li><input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /></li>
+          <li><button type="button" onClick={() => props.signinUser({ email, password }, props.history)}>Sign In</button></li>
+        </ul>
+        <p>Do not have an account? <span id="underline">Sign Up</span></p>
+      </div>
     </div>
   );
 };
 
-export default withRouter(connect(null, { signinUser, signupUser })(SignIn));
+export default withRouter(connect(null, { signinUser })(SignIn));
