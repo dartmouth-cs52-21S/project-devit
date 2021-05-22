b3bba75ab740cdf59a79fbcb1f04ef0db6134148
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-22 17:30:43 -0400

    Use hook to get history

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import Logo from './Logo';
 
-const LandingPage = (props) => {
-  const getStarted = () => {
-    props.history.push('/signup');
-  };
+const LandingPage = () => {
+  const history = useHistory();
+  const getStarted = () => history.push('/signup');
+
   return (
     <div id="landingPageOuter">
       <div id="callToAction">
         <Logo />
         <h1>Connect. Build. Share.</h1>
         <h2>Find your team today</h2>
-        <input type="submit" onClick={getStarted} className="button" value="Get started now" />
+        <button type="button" onClick={getStarted} className="button">Get started now</button>
       </div>
       <img id="biglogo" src="../../images/logo/devit_logo_dark.svg" alt="" />
     </div>
