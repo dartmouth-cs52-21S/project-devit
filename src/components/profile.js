f8d8f250f173e5af0f18afa992cb6526df16fc33
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-22 06:42:17 -0400

    Add component for notifying user when page is protected

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { selectisAuthenticated, selectUser } from '../store/selectors';
+import { selectisAuthenticated } from '../store/selectors';
+import ProtectedPageNotice from './ProtectedPageNotice';
 
 const Profile = () => {
-  const history = useHistory();
-  const user = useSelector(selectUser);
   const isAuthenticated = useSelector(selectisAuthenticated);
-
-  const handleGoToSignIn = () => history.push('/signin');
 
   return (
     <div className="profile">
       <h1 className="profile__heading">Profile</h1>
-      <h2 className="profile__subheading">{user && isAuthenticated ? `Welcome, ${user.author}` : 'You must be signed in to view this page.'}</h2>
-      {!isAuthenticated && <button type="button" className="button" onClick={handleGoToSignIn}>Sign In</button>}
+      {!isAuthenticated ? <ProtectedPageNotice /> : (
+        <h2>This is the content of your profile.</h2>
+      )}
     </div>
   );
 };
