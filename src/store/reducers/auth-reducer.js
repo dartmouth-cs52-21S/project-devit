c4c931c390da07c8b4cc590fa6e4fbf1329eb3c7
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-22 09:36:31 -0400

    Remove error, use toast instead

--- src/store/reducers/auth-reducer.js
+++ src/store/reducers/auth-reducer.js
@@ -11,8 +11,6 @@ const AuthReducer = (state = initialState, action) => {
       return { isAuthenticated: true, user: action.payload };
     case ActionTypes.DEAUTH_USER:
       return { isAuthenticated: false, user: {} };
-    case ActionTypes.AUTH_ERROR:
-      return { isAuthenticated: false, user: state.user };
     default:
       return state;
   }
