9dfcf8b138583a37cd02fa0cc21ecb26499abf7d
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-23 05:37:57 -0400

    Update landing page with 3d logo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import Logo from './Logo';
-
 const LandingPage = () => {
   const history = useHistory();
   const getStarted = () => history.push('/signup');
 
   return (
-    <div id="landingPageOuter">
-      <div id="callToAction">
-        <Logo />
-        <h1>Connect. Build. Share.</h1>
-        <h2>Find your team today</h2>
-        <button type="button" onClick={getStarted} className="button">Get started now</button>
+    <div className="landing">
+      <div className="landing__cta-wrapper">
+        <h1 className="landing__heading">Connect. Build. Share.</h1>
+        <h2 className="landing__subheading">Find your team today</h2>
+        <button type="button" className="button" onClick={getStarted}>Get started now</button>
       </div>
-      <img id="biglogo" src="../../images/logo/devit_logo_dark.svg" alt="" />
+      <div className="landing__image">
+        <img className="landing__logo-3d" src="../images/devit_3d_logo.png" alt="DevIt 3D Logo" />
+      </div>
     </div>
   );
 };
