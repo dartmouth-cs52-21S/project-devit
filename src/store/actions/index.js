92a276ed1912d6ba0dc91f70ba78fc00b3c06ba0
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-25 10:13:09 -0400

    Use async/await syntax for promises

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,105 +5,96 @@ import ActionTypes from '../types';
 
 const ROOT_URL = process.env.REACT_APP_ROOT_URL || 'http://localhost:9090/api';
 
-export const toggleSidebar = () => ({
-  type: ActionTypes.TOGGLE_SIDEBAR,
-});
+export const toggleSidebar = () => ({ type: ActionTypes.TOGGLE_SIDEBAR });
 
 export function fetchProjects() {
-  return (dispatch) => {
-    axios
-      .get(`${ROOT_URL}/projects`).then((response) => {
-        dispatch({ type: ActionTypes.FETCH_PROJECTS, payload: response.data });
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when getting projects.');
-      });
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`${ROOT_URL}/projects`);
+      dispatch({ type: ActionTypes.FETCH_PROJECTS, payload: data });
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when getting projects.');
+    }
   };
 }
 
 export function createProject(project, history) {
-  return (dispatch) => {
-    axios
-      .post(`${ROOT_URL}/projects`, project, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
-        dispatch({ type: ActionTypes.NEW_PROJECT, payload: response.data });
-        history.push('/projects');
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when trying to create your project.');
-      });
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(`${ROOT_URL}/projects`, project, { headers: { authorization: localStorage.getItem('token') } });
+      dispatch({ type: ActionTypes.NEW_PROJECT, payload: data });
+      history.push('/projects');
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when trying to create your project.');
+    }
   };
 }
 
 export function updateProject(project, id) {
-  return (dispatch) => {
-    axios
-      .put(`${ROOT_URL}/projects/${id}`, project, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
-        dispatch({ type: ActionTypes.FETCH_PROJECT, payload: response.data });
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when trying to update that project.');
-      });
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`${ROOT_URL}/projects/${id}`, project, { headers: { authorization: localStorage.getItem('token') } });
+      dispatch({ type: ActionTypes.FETCH_PROJECT, payload: data });
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when trying to update that project.');
+    }
   };
 }
 
 export function fetchProject(id, callback) {
-  return (dispatch) => {
-    axios
-      .get(`${ROOT_URL}/projects/${id}`).then((response) => {
-        dispatch({ type: ActionTypes.FETCH_PROJECT, payload: response.data });
-        callback(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when trying find that project.');
-      });
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`${ROOT_URL}/projects/${id}`);
+      dispatch({ type: ActionTypes.FETCH_PROJECT, payload: data });
+      callback(data);
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when trying find that project.');
+    }
   };
 }
 
 export function deleteProject(id, history) {
-  return (dispatch) => {
-    axios
-      .delete(`${ROOT_URL}/projects/${id}`, { headers: { authorization: localStorage.getItem('token') } }).then(() => {
-        history.push('/');
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when trying to delete that project.');
-      });
+  return async () => {
+    try {
+      const { data } = await axios.delete(`${ROOT_URL}/projects/${id}`, { headers: { authorization: localStorage.getItem('token') } });
+      console.log('data:', data);
+      history.push('/');
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when trying to delete that project.');
+    }
   };
 }
 
 export function signInUser(user, history) {
-  return (dispatch) => {
-    axios
-      .post(`${ROOT_URL}/signin`, user).then((response) => {
-        dispatch({ type: ActionTypes.AUTH_USER, payload: response.data });
-        localStorage.setItem('token', response.data.token);
-        history.push('/');
-      })
-      .catch((error) => {
-        console.error(error);
-        const toastMessage = `Sign In Failed: ${error.response.data}`;
-        toast.dark(toastMessage);
-      });
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(`${ROOT_URL}/signin`, user);
+      dispatch({ type: ActionTypes.AUTH_USER, payload: data });
+      localStorage.setItem('token', data.token);
+      history.push('/profile');
+    } catch (error) {
+      console.error(error);
+      const toastMessage = `Sorry, there was an issue when trying to sign you in: ${error.response.data}`;
+      toast.dark(toastMessage);
+    }
   };
 }
 
 export function signUpUser(user, history) {
-  return (dispatch) => {
-    axios
-      .post(`${ROOT_URL}/signup`, user).then((response) => {
-        dispatch({ type: ActionTypes.AUTH_USER, payload: response.data });
-        localStorage.setItem('token', response.data.token);
-        history.push('/');
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.dark('Sorry, there was an issue when trying to sign you up.');
-      });
+  return async () => {
+    try {
+      const { data } = await axios.post(`${ROOT_URL}/signup`, user);
+      localStorage.setItem('token', data.token);
+      history.push('/profile');
+    } catch (error) {
+      console.error(error);
+      toast.dark('Sorry, there was an issue when trying to sign you up.');
+    }
   };
 }
 
@@ -112,6 +103,6 @@ export function signOutUser(history) {
     localStorage.removeItem('token');
     dispatch({ type: ActionTypes.DEAUTH_USER });
     toast.dark('✌️ You have signed out');
-    history.push('/');
+    history.push('/signin');
   };
 }
