f6c97d26ad32e15f7fd39e492195dc792f900489
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-27 05:55:19 -0400

    Update UserAvatar component to handle a passed in user

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -2,25 +2,26 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectUser, selectUserIsDefined } from '../store/selectors';
+import { pluckFirstLetter } from '../utils/utilityFunctions';
 
-export const UserAvatar = () => {
-  const { firstName, lastName, roles, picture } = useSelector(selectUser);
-  const userIsDefined = useSelector(selectUserIsDefined);
+export const UserAvatar = ({ useAuthenticatedUser, passedInUser }) => {
+  const authenticatedUser = useSelector(selectUser);
+  const authenticatedUserIsDefined = useSelector(selectUserIsDefined);
 
-  if (!userIsDefined) return null;
+  if (useAuthenticatedUser && !authenticatedUserIsDefined) return null;
 
-  const pluckFirstLetter = (string) => string.slice(0, 1);
+  const user = useAuthenticatedUser ? authenticatedUser : passedInUser;
 
-  const userInitials = `${pluckFirstLetter(firstName)}${pluckFirstLetter(lastName)}`;
+  const userInitials = `${pluckFirstLetter(user.firstName)}${pluckFirstLetter(user.lastName)}`;
 
-  const userAvatarClasses = ['user-avatar', ...roles].join(' ');
+  const userAvatarClasses = ['user-avatar', ...user.roles].join(' ');
 
   return (
     <div className={userAvatarClasses}>
-      {picture ? (
-        <img className="user-avatar__image" src={picture} alt={`${[firstName, lastName].join(' ')}`} />
+      {user.picture ? (
+        <img className="user-avatar__image" src={user.picture} alt={`${[user.firstName, user.lastName].join(' ')}`} />
       ) : (
-        <div className="user-avatar__image">{userInitials}</div>
+        <div className="user-avatar__initials">{userInitials}</div>
       )}
     </div>
   );
