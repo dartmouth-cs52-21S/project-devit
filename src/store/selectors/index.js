6345658013aef36b639ea3c93fff1e5da30e0400
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-05-28 11:23:32 -0400

    file duplicates

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,8 +1,16 @@
-// Posts Reducer
-export const selectAllPosts = (state) => state.posts.all;
-export const selectCurrentPost = (state) => state.posts.current;
-export const selectError = (state) => state.posts.error;
+// Projects Reducer
+export const selectAllProjects = (state) => state.projects.all;
+export const selectCurrentProject = (state) => state.projects.current;
+export const selectError = (state) => state.projects.error;
 
 // Auth Reducer
-export const selectAuthenticated = (state) => state.auth.authenticated;
+export const selectisAuthenticated = (state) => state.auth.isAuthenticated;
 export const selectUser = (state) => state.auth.user;
+export const selectUserIsDefined = (state) => Object.keys(state.auth.user).length > 0;
+
+// Sidebar Reducer
+export const selectSidebarIsCollapsed = (state) => state.sidebar.isCollapsed;
+
+// Modal Reducer
+export const selectModalContent = (state) => state.modal.modalContent;
+export const selectModalContentExists = (state) => Object.keys(state.modal.modalContent).length > 0;
