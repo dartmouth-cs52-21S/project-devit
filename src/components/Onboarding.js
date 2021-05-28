22b99ed43a99936426e1bbfb0f654f5570fc8265
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-05-27 22:07:40 -0400

    finished basic styling

---
+++ src/components/Onboarding.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { selectUser } from '../store/selectors';
+
+import { updateUser } from '../store/actions';
+import { uploadImage } from '../store/s3';
+import DarkBG from './DarkBG';
+
+const SelectField = ({
+  user, userArrayKey, handleUpdateUserArray, fieldName, label,
+}) => {
+  const checked = user[userArrayKey]?.includes(fieldName);
+
+  return (
+    <label className={`form__label checkbox-label ${checked ? 'checked' : ''}`} htmlFor={fieldName}>
+      <p className="form__label-text checkbox-label-text">{label}</p>
+      <input
+        id={fieldName}
+        className="form__checkbox"
+        type="checkbox"
+        value={fieldName}
+        checked={checked}
+        onChange={(e) => handleUpdateUserArray(e, user, userArrayKey)}
+      />
+    </label>
+  );
+};
+
+const Onboarding = () => {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    location: '',
+    picture: '../images/user.png',
+    bio: '',
+    roles: [],
+    skills: [],
+  });
+  const [file, setFile] = useState();
+
+  const storedUser = useSelector(selectUser);
+
+  useEffect(() => {
+    let url = '../images/user.png';
+    if (file) { url = window.URL.createObjectURL(file); }
+    setUser({ ...user, picture: url });
+  }, [file]);
+
+  console.log(user);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleUpdateUser = () => {
+    console.log('going to update');
+    console.log('stored user', storedUser);
+    if (file) {
+      uploadImage(file).then((url) => {
+        dispatch(updateUser(storedUser._id, { ...user, picture: url }, history));
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+  };
+
+  const handleUpdateUserArray = (e, userObject, userArrayKey) => {
+    console.log('userobj', userObject);
+    let newArray = [];
+    const existingRole = userObject[userArrayKey]?.includes(e.target.value);
+
+    if (existingRole) {
+      newArray = userObject[userArrayKey].filter((value) => value !== e.target.value);
+    } else {
+      newArray = [...userObject[userArrayKey], e.target.value];
+    }
+
+    setUser({ ...userObject, [userArrayKey]: newArray });
+  };
+
+  const onImageUpload = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const rolesFields = [
+    { fieldName: 'developer', label: 'Developer' },
+    { fieldName: 'designer', label: 'Designer' },
+    { fieldName: 'ideator', label: 'Ideator' },
+  ];
+
+  const skillsFields = [
+    { fieldName: 'react', label: 'React' },
+    { fieldName: 'htmlcss', label: 'HTML/CSS' },
+  ];
+
+  return (
+    <DarkBG>
+      <div className="onboarding">
+        <div className="profile-info">
+          <div className="user-picture">
+            <img src={user.picture} alt="user avatar" />
+            <input type="file" name="coverImage" onChange={(e) => onImageUpload(e)} />
+          </div>
+          <div className="user-details">
+            <h1>User Details</h1>
+            <div>
+              <input type="text" value={user.firstName} placeholder="First Name" onChange={(e) => setUser({ ...user, firstName: e.target.value })} />
+              <input type="text" value={user.lastName} placeholder="Last Name" onChange={(e) => setUser({ ...user, lastName: e.target.value })} />
+            </div>
+            <input type="text"
+              id="long"
+              value={user.location}
+              placeholder={`${<FontAwesomeIcon icon={faMapMarkerAlt} />}Add Location`}
+              onChange={(e) => setUser({ ...user, location: e.target.value })}
+            />
+            <textarea type="text" value={user.bio} placeholder="Bio" onChange={(e) => setUser({ ...user, bio: e.target.value })} />
+          </div>
+        </div>
+        <div className="user-attributes">
+          <section className="form__section">
+            <h1>Roles</h1>
+            <div className="form__checkbox-group">
+              {rolesFields.map(({ fieldName, label }) => (
+                <SelectField key={fieldName} user={user} userArrayKey="roles" handleUpdateUserArray={handleUpdateUserArray} fieldName={fieldName} label={label} />
+              ))}
+            </div>
+          </section>
+          <section className="form__section">
+            <h1>Skills</h1>
+            <div className="form__checkbox-group">
+              {skillsFields.map(({ fieldName, label }) => (
+                <SelectField key={fieldName} user={user} userArrayKey="skills" handleUpdateUserArray={handleUpdateUserArray} fieldName={fieldName} label={label} />
+              ))}
+            </div>
+          </section>
+        </div>
+
+        <button type="button"
+          onClick={() => handleUpdateUser()}
+        >Next
+        </button>
+      </div>
+    </DarkBG>
+  );
+};
+
+export default Onboarding;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,159 +1,53 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
-import { RiCheckboxFill, RiCheckboxBlankLine } from 'react-icons/ri';
 import { signUpUser } from '../store/actions';
-import { uploadImage } from '../store/s3';
 import DarkBG from './DarkBG';
 
-const SelectField = ({
-  user, userArrayKey, handleUpdateUserArray, fieldName, label,
-}) => {
-  const checked = user[userArrayKey].includes(fieldName);
-
-  return (
-    <label className={`form__label checkbox-label ${checked ? 'checked' : ''}`} htmlFor={fieldName}>
-      <span className="checkbox-icon">
-        {checked ? <RiCheckboxFill /> : <RiCheckboxBlankLine />}
-      </span>
-      <p className="form__label-text checkbox-label-text">{label}</p>
-      <input
-        id={fieldName}
-        className="form__checkbox"
-        type="checkbox"
-        value={fieldName}
-        checked={checked}
-        onChange={(e) => handleUpdateUserArray(e, user, userArrayKey)}
-      />
-    </label>
-  );
-};
-
 const SignUp = () => {
-  const [file, setFile] = useState();
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: '',
-    location: '',
-    picture: '',
-    bio: '',
-    roles: [],
-    skills: [],
-  });
-
-  console.log(user);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const handleSignUpUser = () => {
-    if (file) {
-      uploadImage(file).then((url) => {
-        dispatch(signUpUser({ ...user, picture: url }, history));
-      }).catch((error) => {
-        console.error(error);
-      });
-    }
+  const readyToSubmit = email && password && confirmPassword;
+
+  const handleSignUpUser = (e) => {
+    e.preventDefault();
+    dispatch(signUpUser({ email, password }, history));
+    history.push('/onboarding');
   };
 
-  const handleUpdateUserArray = (e, userObject, userArrayKey) => {
-    let newArray = [];
-    const existingRole = userObject[userArrayKey].includes(e.target.value);
-
-    if (existingRole) {
-      newArray = userObject[userArrayKey].filter((value) => value !== e.target.value);
-    } else {
-      newArray = [...userObject[userArrayKey], e.target.value];
-    }
-
-    setUser({ ...userObject, [userArrayKey]: newArray });
-  };
-
-  const onImageUpload = (event) => {
-    setFile(event.target.files[0]);
-    if (file) {
-      setUser({ picture: window.URL.createObjectURL(file) });
-    }
-  };
-
-  const rolesFields = [
-    { fieldName: 'developer', label: 'Developer' },
-    { fieldName: 'designer', label: 'Designer' },
-    { fieldName: 'ideator', label: 'Ideator' },
-  ];
-
-  const skillsFields = [
-    { fieldName: 'react', label: 'React' },
-    { fieldName: 'htmlcss', label: 'HTML/CSS' },
-  ];
+  const handleUpdateEmail = (e) => setEmail(e.target.value);
+  const handleUpdatePassword = (e) => setPassword(e.target.value);
+  const handleUpdateConfirmPassword = (e) => setConfirmPassword(e.target.value);
 
   return (
     <DarkBG>
-      <div className="sign-up">
-        <h1>Account Info</h1>
-        <ul>
-          <li><h2>Email:<span id="red">*</span> </h2></li>
-          <li><input type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} /></li>
-          <li><h2>Password:<span id="red">*</span> </h2></li>
-          <li><input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} /></li>
-        </ul>
-        <h1>User Info</h1>
-        <ul>
-          <li><h2>First Name:<span id="red">*</span> </h2></li>
-          <li><input type="text" value={user.firstName} onChange={(e) => setUser({ ...user, firstName: e.target.value })} /></li>
-          <li><h2>Last Name:<span id="red">*</span> </h2></li>
-          <li><input type="text" value={user.lastName} onChange={(e) => setUser({ ...user, lastName: e.target.value })} /></li>
-        </ul>
-        <ul>
-          <li><h2>Profile Pic: </h2></li>
-          <li><input type="file" name="coverImage" onChange={(e) => onImageUpload(e)} /></li>
-
-          <li><h2>Location: </h2></li>
-          <li><input type="text" value={user.location} onChange={(e) => setUser({ ...user, location: e.target.value })} /></li>
-        </ul>
-        <ul>
-          <li><h2>Bio: </h2></li>
-          <li><textarea type="text" value={user.bio} onChange={(e) => setUser({ ...user, bio: e.target.value })} /></li>
-        </ul>
-        {/* <ul>
-          <li><h2>Roles: </h2></li>
-          <li>
-            <button type="button" onClick={() => setUser({ ...user, roles: user.roles.add('dev') })}>dev</button>
-            <button type="button" onClick={() => setUser({ ...user, roles: [...user.roles, 'designer'] })}>designer</button>
-            <button type="button" onClick={() => setUser({ ...user, roles: [...user.roles, 'ideator'] })}>ideator</button>
-          </li>
-        </ul>
-        <ul>
-          <li><h2>Skills: </h2></li>
-          <li>
-            <button type="button" onClick={() => setUser({ ...user, skills: [...user.skills, 'react'] })}>React</button>
-            <button type="button" onClick={() => setUser({ ...user, skills: [...user.skills, 'html/css'] })}>html/css</button>
-          </li>
-        </ul> */}
-        <section className="form__section">
-          <h3 className="form__section-heading">Roles</h3>
-          <div className="form__checkbox-group">
-            {rolesFields.map(({ fieldName, label }) => (
-              <SelectField key={fieldName} user={user} userArrayKey="roles" handleUpdateUserArray={handleUpdateUserArray} fieldName={fieldName} label={label} />
-            ))}
-          </div>
-        </section>
-        <section className="form__section">
-          <h3 className="form__section-heading">Skills</h3>
-          <div className="form__checkbox-group">
-            {skillsFields.map(({ fieldName, label }) => (
-              <SelectField key={fieldName} user={user} userArrayKey="skills" handleUpdateUserArray={handleUpdateUserArray} fieldName={fieldName} label={label} />
-            ))}
-          </div>
-        </section>
-        <button type="button"
-          onClick={() => handleSignUpUser}
-        >Sign Up
-        </button>
-      </div>
+      <section className="sign-in form">
+        <div className="form__container">
+          <h2 className="form__heading">Sign Up</h2>
+          <form className="form__form" onSubmit={handleSignUpUser}>
+            <label className="form__label" htmlFor="email">
+              <p className="form__label-text">Email<span className="form__required">*</span></p>
+              <input className="form__label-input" type="text" value={email} onChange={handleUpdateEmail} />
+            </label>
+            <label className="form__label" htmlFor="password">
+              <p className="form__label-text">Password<span className="form__required">*</span></p>
+              <input className="form__label-input" type="password" value={password} onChange={handleUpdatePassword} />
+            </label>
+            <label className="form__label" htmlFor="password">
+              <p className="form__label-text">Confirm Password<span className="form__required">*</span></p>
+              <input className="form__label-input" type="password" value={confirmPassword} onChange={handleUpdateConfirmPassword} />
+            </label>
+            <button type="submit" className="button form__button" disabled={!readyToSubmit}>Sign Up</button>
+          </form>
+          <p>Already have an account? <Link to="/signin">Sign In</Link></p>
+        </div>
+      </section>
     </DarkBG>
   );
 };
