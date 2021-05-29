24a8af2e16db75a70382c5bb05633c35df0adf8e
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-29 18:45:54 -0400

    Update UserAvatar, return an empty string if names are not defined

--- src/utils/utilityFunctions.js
+++ src/utils/utilityFunctions.js
@@ -1,4 +1,7 @@
-export const pluckFirstLetter = (string) => string.slice(0, 1);
+export const getInitials = (firstName, lastName) => {
+  if (!firstName || !lastName) return '';
+  return `${firstName.slice(0, 1)}${lastName.slice(0, 1)}`;
+};
 
 // placeholder so linter won't complain about not using default export
 export const function2 = () => {};
