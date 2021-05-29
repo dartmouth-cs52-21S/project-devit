b6b29b86d0fe9567b7457df83ca282c4b7a46711
henry <henry.b.mans.23@dartmouth.edu>
2021-05-29 14:13:06 -0400

    commented out the avatar because it was causing some errors in testing

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-import { selectUser, selectUserIsDefined } from '../store/selectors';
+// import { selectUser, selectUserIsDefined } from '../store/selectors';
 
 export const UserAvatar = () => {
-  const { firstName, lastName, roles, picture } = useSelector(selectUser);
-  const userIsDefined = useSelector(selectUserIsDefined);
+  // const { firstName, lastName, roles, picture } = useSelector(selectUser);
+  // const userIsDefined = useSelector(selectUserIsDefined);
 
-  if (!userIsDefined) return null;
+  // if (!userIsDefined) return null;
 
   // const pluckFirstLetter = (string) => string.slice(0, 1);
 
   // const userInitials = `${pluckFirstLetter(firstName)}${pluckFirstLetter(lastName)}`;
 
-  const userAvatarClasses = ['user-avatar', ...roles].join(' ');
+  // const userAvatarClasses = ['user-avatar', ...roles].join(' ');
 
   return (
-    <div className={userAvatarClasses}>
-      {picture ? (
-        <img className="user-avatar__image" src={picture} alt={`${[firstName, lastName].join(' ')}`} />
-      ) : (
-        <div className="user-avatar__image">Initials</div>
-      )}
+    // <div className={userAvatarClasses}>
+    //   {picture ? (
+    //     // <img className="user-avatar__image" src={picture} alt={`${[firstName, lastName].join(' ')}`} />
+    //     <img className="user-avatar__image" src={picture} alt="firstname lastname" />
+    //   ) : (
+    //     <div className="user-avatar__image">Initials</div>
+    //   )}
+    // </div>
+    <div>
+      user avatar
     </div>
   );
 };
