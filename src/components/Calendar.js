3ac04363b010ad542be111dab87bfc9af06c72c0
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2021-05-30 22:58:47 -0400

    calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,29 +1,66 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-alert */
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const ToDo = () => {
+  const [events, setEvents] = useState([
+    {
+      title: 'Team Meeting',
+      start: '2021-05-30',
+    },
+    {
+      title: 'Dev Site Launch',
+      start: '2021-06-02',
+    },
+  ]);
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState(new Date());
+
+  const handleEventClick = (eventClickInfo) => {
+    setEvents(events.filter((e) => {
+      return e.title !== eventClickInfo.event._def.title;
+    }));
+  };
+
+  const addEvent = () => {
+    setEvents([...events, {
+      title,
+      start: date,
+    }]);
+    setTitle('');
+    setDate(new Date());
+  };
+
   return (
-    <div>
-      <h1>Cal</h1>
+    <div className="calendar">
+      <h1>Calendar</h1>
+      <div className="events">
+        <label htmlFor="title">
+          Title:
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </label>
+        <label htmlFor="date">Date:
+          <input type="date"
+            id="date"
+            name="trip-start"
+            value={date}
+            min="2021-01-01"
+            max="2022-01-01"
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={addEvent}>Add Event</button>
+      </div>
       <FullCalendar
-        plugins={[listPlugin]}
+        plugins={[listPlugin, interactionPlugin]}
         initialView="listWeek"
-        events={[
-          {
-            title: 'Meeting',
-            start: '2021-05-27T14:30:00',
-            extendedProps: {
-              status: 'done',
-            },
-          },
-          {
-            title: 'Birthday Party',
-            start: '2021-05-28T07:00:00',
-            backgroundColor: 'green',
-            borderColor: 'green',
-          },
-        ]}
+        editable
+        eventClick={handleEventClick}
+        defaultAllDay
+        events={events}
       />
     </div>
   );
