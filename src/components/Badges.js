b866bf201fb491875cce6f2c383234f40c328779
henry <henry.b.mans.23@dartmouth.edu>
2021-05-31 23:04:58 -0400

    added tiers of the currently existing badges

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -11,37 +11,77 @@ import {
 import { Tooltip } from '@material-ui/core';
 
 const badgesObject = {
-  ideatorBeginner: {
+  ideatorBronze: {
     icon: <FontAwesomeIcon icon={faLightbulb} color="sienna" />,
     hoverText: 'Create a Project',
   },
-  ideatorPro: {
-    icon: <FontAwesomeIcon icon={faLightbulb} color="yellow" />,
+  ideatorSilver: {
+    icon: <FontAwesomeIcon icon={faLightbulb} color="silver" />,
     hoverText: 'Create 3 Projects',
   },
-  devitBeginner: {
+  ideatorGold: {
+    icon: <FontAwesomeIcon icon={faLightbulb} color="gold" />,
+    hoverText: 'Create 5 Projects',
+  },
+  devitBronze: {
     icon: <FontAwesomeIcon icon={faLaptop} color="sienna" />,
     hoverText: 'Join a Project',
   },
-  devitPro: {
-    icon: <FontAwesomeIcon icon={faLaptop} color="yellow" />,
+  devitSilver: {
+    icon: <FontAwesomeIcon icon={faLaptop} color="silver" />,
     hoverText: 'Join 3 Projects',
   },
-  githubCommits: {
-    icon: <FontAwesomeIcon icon={faGithub} />,
-    hoverText: 'Make 15 commits with github',
+  devitGold: {
+    icon: <FontAwesomeIcon icon={faLaptop} color="gold" />,
+    hoverText: 'Join 5 Projects',
   },
-  pullRequestPro: {
-    icon: <FontAwesomeIcon icon={faCodeBranch} />,
+  commitBronze: {
+    icon: <FontAwesomeIcon icon={faGithub} color="sienna" />,
+    hoverText: 'Make 5 commits with github',
+  },
+  commitSilver: {
+    icon: <FontAwesomeIcon icon={faGithub} color="silver" />,
+    hoverText: 'Make 20 commits with github',
+  },
+  commitGold: {
+    icon: <FontAwesomeIcon icon={faGithub} color="gold" />,
+    hoverText: 'Make 50 commits with github',
+  },
+  pullRequestBronze: {
+    icon: <FontAwesomeIcon icon={faCodeBranch} color="sienna" />,
+    hoverText: 'Make 1 pull request with github',
+  },
+  pullRequestSilver: {
+    icon: <FontAwesomeIcon icon={faCodeBranch} color="silver" />,
     hoverText: 'Make 5 pull requests with github',
   },
-  planner: {
-    icon: <FontAwesomeIcon icon={faCalendar} />,
-    hoverText: 'Create 5 events on the Calendar',
+  pullRequestGold: {
+    icon: <FontAwesomeIcon icon={faCodeBranch} color="gold" />,
+    hoverText: 'Make 15 pull requests with github',
   },
-  activeChatter: {
-    icon: <FontAwesomeIcon icon={faComments} />,
+  plannerBronze: {
+    icon: <FontAwesomeIcon icon={faCalendar} color="sienna" />,
+    hoverText: 'Create 1 event on the Calendar',
+  },
+  plannerSilver: {
+    icon: <FontAwesomeIcon icon={faCalendar} color="silver" />,
+    hoverText: 'Create 3 events on the Calendar',
+  },
+  plannerGold: {
+    icon: <FontAwesomeIcon icon={faCalendar} color="gold" />,
+    hoverText: 'Create 10 events on the Calendar',
+  },
+  chatterBronze: {
+    icon: <FontAwesomeIcon icon={faComments} color="sienna" />,
+    hoverText: 'Send 10 messages in the chat',
+  },
+  chatterSilver: {
+    icon: <FontAwesomeIcon icon={faComments} color="silver" />,
     hoverText: 'Send 25 messages in the chat',
+  },
+  chatterGold: {
+    icon: <FontAwesomeIcon icon={faComments} color="gold" />,
+    hoverText: 'Send 50 messages in the chat',
   },
 };
 
