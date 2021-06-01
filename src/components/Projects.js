3f4e2ebb186e1efd7dcc95f8c23c9cda2ef167aa
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-06-01 15:01:37 -0400

    Use properties from project object to populate template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { FaArrowRight } from 'react-icons/fa';
+import dayjs from 'dayjs';
+
 import { selectUser } from '../store/selectors/index';
 
+const relativeTime = require('dayjs/plugin/relativeTime');
+
 const Projects = () => {
+  dayjs.extend(relativeTime);
+
+  const history = useHistory();
   const { projects } = useSelector(selectUser);
+
+  const handleGoToProject = (id) => history.push(`/projects/${id}`);
 
   return (
     <div className="projects">
@@ -12,27 +23,38 @@ const Projects = () => {
         <p className="projects__empty-message">Looks like you don&apos;t have any projects yet</p>
       ) : (
         <ul className="projects__projects-list">
-          <li className="projects__project">
-            <div className="projects__project-icon">🎉</div>
-            <div className="projects__project-body">
-              <h3 className="projects__project-name">LiveWell</h3>
-              <div className="projects__project-details-wrapper">
-                <p className="projects__project-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro natus nostrum, tempore incidunt dignissimos aperiam nemo officia quas?</p>
-                <ul className="projects__project-industries">
-                  <li className="projects__project-industry">Health Care</li>
-                  <li className="projects__project-industry">Tech</li>
-                  <li className="projects__project-industry">Crypto</li>
-                  <li className="projects__more-label">+3 more</li>
-                </ul>
-                <div className="projects__project-originally-posted">
-                  <span className="projects__originally-posted-text">Originally posted</span>
-                  <span className="projects__originally-posted-date">6 days ago</span>
-                  <span className="projects__originally-posted-text">by</span>
-                  <span className="projects__originally-posted-by">Aaron Morgan</span>
+          {projects.map((project) => (
+            <li key={project.id} className="projects__project">
+              <div className="projects__project-icon">{project.logo}</div>
+              <div className="projects__project-body">
+                <h3 className="projects__project-name">{project.name}</h3>
+                <div className="projects__project-details-wrapper">
+                  <p className="projects__project-description">{project.bio}</p>
+                  <ul className="projects__project-industries">
+                    {project.industry.slice(0, 3).map((industry) => (
+                      <li key={industry} className="projects__project-industry">{industry}</li>
+                    ))}
+                    {project.industry.slice(3).length > 0 && (
+                      <li className="projects__more-label">{`+${project.industry.slice(3).length} more`}</li>
+                    )}
+                  </ul>
+                  <div className="projects__project-originally-posted">
+                    <span className="projects__originally-posted-text">Originally posted</span>
+                    <span className="projects__originally-posted-date">{dayjs(project.createdAt).fromNow()}</span>
+                    <span className="projects__originally-posted-text">by</span>
+                    <span className="projects__originally-posted-by">{`${project.author.firstName} ${project.author.lastName}`}</span>
+                  </div>
+                  <button
+                    type="button"
+                    className="button view-project-button ghost small"
+                    onClick={() => handleGoToProject(project.id)}
+                  >
+                    <FaArrowRight />
+                  </button>
                 </div>
               </div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       )}
     </div>
