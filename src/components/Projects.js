3d9a89261a9229ba6dfe233322eea72aba037d9a
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-06-01 15:09:23 -0400

    Abstract out rendering of project to separate, shareable component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,7 @@ import { selectUser } from '../store/selectors/index';
 const relativeTime = require('dayjs/plugin/relativeTime');
 
 const Projects = () => {
-  dayjs.extend(relativeTime);
-
-  const history = useHistory();
   const { projects } = useSelector(selectUser);
-
-  const handleGoToProject = (id) => history.push(`/projects/${id}`);
 
   return (
     <div className="projects">
@@ -24,36 +19,7 @@ const Projects = () => {
       ) : (
         <ul className="projects__projects-list">
           {projects.map((project) => (
-            <li key={project.id} className="projects__project">
-              <div className="projects__project-icon">{project.logo}</div>
-              <div className="projects__project-body">
-                <h3 className="projects__project-name">{project.name}</h3>
-                <div className="projects__project-details-wrapper">
-                  <p className="projects__project-description">{project.bio}</p>
-                  <ul className="projects__project-industries">
-                    {project.industry.slice(0, 3).map((industry) => (
-                      <li key={industry} className="projects__project-industry">{industry}</li>
-                    ))}
-                    {project.industry.slice(3).length > 0 && (
-                      <li className="projects__more-label">{`+${project.industry.slice(3).length} more`}</li>
-                    )}
-                  </ul>
-                  <div className="projects__project-originally-posted">
-                    <span className="projects__originally-posted-text">Originally posted</span>
-                    <span className="projects__originally-posted-date">{dayjs(project.createdAt).fromNow()}</span>
-                    <span className="projects__originally-posted-text">by</span>
-                    <span className="projects__originally-posted-by">{`${project.author.firstName} ${project.author.lastName}`}</span>
-                  </div>
-                  <button
-                    type="button"
-                    className="button view-project-button ghost small"
-                    onClick={() => handleGoToProject(project.id)}
-                  >
-                    <FaArrowRight />
-                  </button>
-                </div>
-              </div>
-            </li>
+            <ProjectCard key={project.id} project={project} />
           ))}
         </ul>
       )}
@@ -62,3 +28,44 @@ const Projects = () => {
 };
 
 export default Projects;
+
+export const ProjectCard = ({ project }) => {
+  dayjs.extend(relativeTime);
+
+  const history = useHistory();
+
+  const handleGoToProject = (id) => history.push(`/projects/${id}`);
+
+  return (
+    <li className="projects__project">
+      <div className="projects__project-icon">{project.logo}</div>
+      <div className="projects__project-body">
+        <h3 className="projects__project-name">{project.name}</h3>
+        <div className="projects__project-details-wrapper">
+          <p className="projects__project-description">{project.bio}</p>
+          <ul className="projects__project-industries">
+            {project.industry.slice(0, 3).map((industry) => (
+              <li key={industry} className="projects__project-industry">{industry}</li>
+            ))}
+            {project.industry.slice(3).length > 0 && (
+              <li className="projects__more-label">{`+${project.industry.slice(3).length} more`}</li>
+            )}
+          </ul>
+          <div className="projects__project-originally-posted">
+            <span className="projects__originally-posted-text">Originally posted</span>
+            <span className="projects__originally-posted-date">{dayjs(project.createdAt).fromNow()}</span>
+            <span className="projects__originally-posted-text">by</span>
+            <span className="projects__originally-posted-by">{`${project.author.firstName} ${project.author.lastName}`}</span>
+          </div>
+          <button
+            type="button"
+            className="button view-project-button ghost small"
+            onClick={() => handleGoToProject(project.id)}
+          >
+            <FaArrowRight />
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+};
