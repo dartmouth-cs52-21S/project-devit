8081d5fdf996369e9c3aeb0f03a009e6726e8998
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-06-07 06:13:52 -0400

    Use global button styles

--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -108,20 +108,19 @@ const NewProject = () => {
     { fieldName: 'developer', label: 'Developers' },
     { fieldName: 'designer', label: 'Designers' },
   ];
-
   return (
     <form className="form__form" onSubmit={makeProject}>
       <div id="new-project">
         {editEmoji
           ? (
             <div className="picker">
-              <Picker onEmojiClick={onEmojiClick} pickerStyle={{ width: '40%', backgroundColor: '#3B3B3B' }} />
-              <button className="save" type="button" onClick={() => setEditEmoji(false)}>Done!</button>
+              <Picker onEmojiClick={onEmojiClick} preload />
+              <button className="button save" type="button" onClick={() => setEditEmoji(false)}>Done!</button>
             </div>
           )
           : (
             <div className="title">
-              <button type="button" className="emoji" onClick={() => setEditEmoji(true)}>{chosenEmoji.emoji}</button>
+              <button type="button" className="button emoji" onClick={() => setEditEmoji(true)}>{chosenEmoji.emoji}</button>
               <input className="title" placeholder="Project title..." type="text" name="title" value={formik.values.title} onChange={formik.handleChange} />
             </div>
           )}
@@ -151,7 +150,7 @@ const NewProject = () => {
               ? (
                 <div className="add-ind">
                   <input className="add" placeholder="industry" type="text" onChange={(e) => setWorkingIndustry(e.target.value)} />
-                  <button className="add" type="submit" onClick={handleIndustry}>Add</button>
+                  <button className="button add" type="submit" onClick={handleIndustry}>Add</button>
                 </div>
               )
               : <button className="add" type="button" onClick={() => setEditIndustry(true)}>+</button>}
@@ -169,7 +168,6 @@ const NewProject = () => {
                     <input className="form__checkbox" type="checkbox" id={fieldName} onChange={() => addRole(fieldName)} />
                   </label>
                 </div>
-
               );
             })}
           </div>
@@ -187,7 +185,7 @@ const NewProject = () => {
           <textarea placeholder="Describe your plan" rows="4" columns="50" name="marketDescription" value={formik.values.marketDescription} onChange={formik.handleChange} />
         </div>
         <div className="buttons">
-          <button className="save" type="submit" disabled={!(formik.isValid && formik.dirty)}>Share</button>
+          <button className="button save" type="submit" disabled={!(formik.isValid && formik.dirty)}>Share</button>
         </div>
       </div>
     </form>
